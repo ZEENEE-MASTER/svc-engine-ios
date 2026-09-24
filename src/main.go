40bda46058adafad527db4fcb3b3f6d5194e7795
ZEENEE-MASTER <zeenee@local>
2026-09-24 12:38:20 -0400

    iOS: native display size, -nojoy (bridge owns input)

--- src/main.go
+++ src/main.go
@@ -196,6 +196,9 @@ func realMain() {
 	if runtime.GOOS == "ios" {
 		cfg.Video.RenderMode = "Vulkan 1.3"
 	}
+	if runtime.GOOS == "ios" {
+		cfg.Video.RenderMode = "Vulkan 1.3"
+	}
 	sys.cfg = *cfg
 	// Logcat("LOG: Config Loaded. System Script: " + sys.cfg.Config.System)
 
--- src/system_sdl.go
+++ src/system_sdl.go
@@ -70,10 +70,18 @@ func (s *System) newWindow(w, h int) (*Window, error) {
 		if runtime.GOOS == "ios" {
 			fullscreen = true
 		}
+		if runtime.GOOS == "ios" {
+			fullscreen = true
+		}
 
 		// Override default sizes if config specifies
 		if sys.cfg.Video.WindowWidth > 0 || sys.cfg.Video.WindowHeight > 0 {
 			w2, h2 = int32(sys.cfg.Video.WindowWidth), int32(sys.cfg.Video.WindowHeight)
+		}
+		if runtime.GOOS == "ios" && fullscreen {
+			if db, dberr := sdl.GetDisplayBounds(0); dberr == nil {
+				w2, h2 = db.W, db.H
+			}
 		}
 
 		if runtime.GOOS != "android" && runtime.GOOS != "ios" {
